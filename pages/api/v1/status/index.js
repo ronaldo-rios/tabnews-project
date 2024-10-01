435603ee369e6ae865ad5a0c8e45fe7316c18c01
Ronaldo <ronaldo-troupe@bitbucket.org>
2024-10-01 17:50:07 -0300

    feature:Add jsconfig, teste de integração configuração de database e retorno de dados do postgre

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
+import database from "/infra/database";
 
-function status(request, response) {
-    const updatedAt = new Date().toISOString();
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
 
-    response.status(200).json({
-        updated_at: updatedAt,
-    });
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseOpenedConnections = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = current_database();",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
