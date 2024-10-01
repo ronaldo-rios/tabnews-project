70499eedb54f54129bc8a77c7d3c0600a50cfe67
Ronaldo <ronaldo-troupe@bitbucket.org>
2024-10-01 17:50:07 -0300

    feature:Add jsconfig, teste de integração configuração de database e retorno de dados do postgre

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,25 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObj) {
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        port: process.env.POSTGRES_PORT
-    });
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+
+  try {
     await client.connect();
     const result = await client.query(queryObj);
-    await client.end();
     return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-    query: query
-}
+  query: query,
+};
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,8 +3,13 @@ test("GET to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined()
+  expect(responseBody.updated_at).toBeDefined();
 
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(
+    responseBody.dependencies.database.opened_connections,
+  ).toBeGreaterThanOrEqual(1);
 });
