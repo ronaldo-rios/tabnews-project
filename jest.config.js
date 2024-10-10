c13d3c5edb43e2fcd0912d57720a89207e6cefa6
Ronaldo <ronaldo-troupe@bitbucket.org>
2024-10-10 01:05:15 -0300

    feat: add husky and implements singleQuote  with prettier

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
-const nextJest = require("next/jest")
-const dotenv = require("dotenv")
+const nextJest = require('next/jest')
+const dotenv = require('dotenv')
 
-dotenv.config({ path: ".env.development" })
+dotenv.config({ path: '.env.development' })
 // The Jest environment is set to "test" by default, so we need to set it to "development"
-process.env.NODE_ENV = "development"
+process.env.NODE_ENV = 'development'
 
 const createJestConfig = nextJest({
-  dir: ".",
+  dir: '.',
 })
 
 const jestConfig = createJestConfig({
-  moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleDirectories: ['node_modules', '<rootDir>'],
   testTimeout: 60000,
 })
 
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,17 +1,17 @@
-import database from "infra/database"
-import { BASE_URL } from "tests/integration/api/v1/config.integration"
+import database from 'infra/database'
+import { BASE_URL } from 'tests/integration/api/v1/config.integration'
 
 const cleanDatabase = async () => {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;")
+  await database.query('DROP SCHEMA public CASCADE; CREATE SCHEMA public;')
 }
 
 beforeAll(async () => await cleanDatabase())
 
-test("POST to api/v1/migrations should return 201", async () => {
+test('POST to api/v1/migrations should return 201', async () => {
   /* When exists pending migrations to run the status code should be 201 
   after run the migrations */
   const response = await fetch(`${BASE_URL}/api/v1/migrations`, {
-    method: "POST",
+    method: 'POST',
   })
   expect(response.status).toBe(201)
 
@@ -20,11 +20,11 @@ test("POST to api/v1/migrations should return 201", async () => {
   expect(responseBody.length).toBeGreaterThan(0)
 })
 
-test("POST to api/v1/migrations should return 200", async () => {
+test('POST to api/v1/migrations should return 200', async () => {
   /* When not exists pending migrations to run the status code should be 200 
   why the migrations are already up */
   const response = await fetch(`${BASE_URL}/api/v1/migrations`, {
-    method: "POST",
+    method: 'POST',
   })
   expect(response.status).toBe(200)
 
