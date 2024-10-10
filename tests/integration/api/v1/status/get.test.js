43830a67b7a36e6f4af550b68b01fbe1c849329c
Ronaldo <ronaldo-troupe@bitbucket.org>
2024-10-10 17:15:02 -0300

    test: adjust tests describe and error with join migrations

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,17 +1,19 @@
 import { BASE_URL } from 'tests/integration/api/v1/config.integration'
 
-test('GET to api/v1/status should return 200', async () => {
-  const response = await fetch(`${BASE_URL}/api/v1/status`)
-  expect(response.status).toBe(200)
+describe('GET to api/v1/status', () => {
+  test('Retrieving current system status', async () => {
+    const response = await fetch(`${BASE_URL}/api/v1/status`)
+    expect(response.status).toBe(200)
 
-  const responseBody = await response.json()
-  expect(responseBody.updated_at).toBeDefined()
+    const responseBody = await response.json()
+    expect(responseBody.updated_at).toBeDefined()
 
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString()
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt)
-  expect(responseBody.dependencies.database.version).toEqual('16.0')
-  expect(responseBody.dependencies.database.max_connections).toEqual(100)
-  expect(
-    responseBody.dependencies.database.opened_connections,
-  ).toBeGreaterThanOrEqual(1)
+    const parseUpdatedAt = new Date(responseBody.updated_at).toISOString()
+    expect(responseBody.updated_at).toEqual(parseUpdatedAt)
+    expect(responseBody.dependencies.database.version).toEqual('16.0')
+    expect(responseBody.dependencies.database.max_connections).toEqual(100)
+    expect(
+      responseBody.dependencies.database.opened_connections,
+    ).toBeGreaterThanOrEqual(1)
+  })
 })
